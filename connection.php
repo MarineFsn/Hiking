03cef4ffdfe7ee10baec52ccc37bdc2245708f32
Lysie <Lysie.a.soyez@gmail.com>
2023-06-14 16:13:20 +0200

    files setting and connection SQL

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?PHP
+
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=becode;charset=utf8', 'root', '');
+} catch (Exception $e) {
+    die('Erreur: ' . $e->getMessage());
+}
+
+
+
+
+
+
+
+
+?>
